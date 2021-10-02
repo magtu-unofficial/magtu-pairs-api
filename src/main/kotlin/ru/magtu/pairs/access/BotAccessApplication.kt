7c4fb679a11260c3ddbb925f8cdcd5e128e2ddf0
elerphore <simon6041@gmail.com>
2021-10-02 09:47:42 +0500

    Rate limits.

--- src/main/kotlin/ru/magtu/pairs/access/BotAccessApplication.kt
+++ src/main/kotlin/ru/magtu/pairs/access/BotAccessApplication.kt
@@ -2,6 +2,7 @@ package ru.magtu.pairs.access
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.config.CorsRegistry
 
@@ -9,6 +10,7 @@ import org.springframework.web.reactive.config.CorsRegistry
 
 
 @SpringBootApplication
+@EnableCaching
 class BotAccessApplication
 
 fun main(args: Array<String>) {
